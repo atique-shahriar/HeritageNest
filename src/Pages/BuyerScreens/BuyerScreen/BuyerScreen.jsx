3390b5f9348fe7739a1cec42238ef7ef436017d1
atique-shahriar <satique06@gmail.com>
2024-06-24 19:09:10 +0600

    Create popular property section

--- src/Pages/BuyerScreens/BuyerScreen/BuyerScreen.jsx
+++ src/Pages/BuyerScreens/BuyerScreen/BuyerScreen.jsx
@@ -2,15 +2,16 @@ import backgroundImage from "../../../../public/Images/BuyerScreen/Banner.jpeg";
 import Banner from "../../../Layouts/Banner/Banner";
 import FindProperty from "../FindProperty/FindProperty";
 import FlatListedCount from "../FlatListedCount/FlatListedCount";
+import PopularProperties from "../PopularProperties/PopularProperties";
 
 const BuyerScreen = () => {
   return (
     <div>
       <Banner backgroundImage={backgroundImage}></Banner>
       <FindProperty></FindProperty>
-
       <div className="mt-[350px] md:mt-48">
         <FlatListedCount></FlatListedCount>
+        <PopularProperties></PopularProperties>
       </div>
     </div>
   );
---
+++ src/Pages/BuyerScreens/PopularProperties/PopularProperties.jsx
@@ -0,0 +1,69 @@
+import img1 from "../../../../public/Images/BuyerScreen/Card (1).jpeg";
+import img2 from "../../../../public/Images/BuyerScreen/Card (2).jpeg";
+import img3 from "../../../../public/Images/BuyerScreen/Card (3).jpeg";
+import PropertyCard from "../../../Components/PropertyCard";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+
+import { FreeMode, Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const PopularProperties = () => {
+  return (
+    <div className="w-11/12 md:w-4/5 mx-auto mt-10 pb-6 border-b-[1px]">
+      <div className="flex justify-between items-center mb-4">
+        <h4 className="text-3xl font-semibold text-[#111827]">Popular Properties</h4>
+        <a
+          href=""
+          className="text-[#0059B1] font-semibold underline">
+          See all Property
+        </a>
+      </div>
+      <div className="relative">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          freeMode={true}
+          modules={[FreeMode, Navigation]}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 30,
+            },
+            1024: {
+              slidesPerView: 3,
+              spaceBetween: 40,
+            },
+          }}
+          className="mySwiper">
+          <SwiperSlide>
+            <PropertyCard cardImage={img3}></PropertyCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <PropertyCard cardImage={img1}></PropertyCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <PropertyCard cardImage={img2}></PropertyCard>
+          </SwiperSlide>
+          <SwiperSlide>
+            <PropertyCard cardImage={img3}></PropertyCard>
+          </SwiperSlide>
+        </Swiper>
+        <div className="swiper-button-prev"></div>
+        <div className="swiper-button-next"></div>
+      </div>
+    </div>
+  );
+};
+
+export default PopularProperties;
