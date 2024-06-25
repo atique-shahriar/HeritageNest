2f96cd865f1462e080273e5e0efe5e32561186c3
atique-shahriar <satique06@gmail.com>
2024-06-26 00:49:20 +0600

    Create property details bid property section

--- src/Pages/PropertyDetails/PropertyDetailsBanner/PropertyDetailsBanner.jsx
+++ src/Pages/PropertyDetails/PropertyDetailsBanner/PropertyDetailsBanner.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import RangeSlider from "react-range-slider-input";
+import "react-range-slider-input/dist/style.css";
 import ImageSection from "./ImageSection";
 import OvervieSection from "./OvervieSection";
 import ToptitleSection from "./ToptitleSection";
 
 const PropertyDetailsBanner = () => {
+  const [value, setValue] = useState([290, 300]);
+  const [tooltipValues, setTooltipValues] = useState([290, 300]);
+
+  const handleInput = (newValue) => {
+    setValue(newValue);
+    setTooltipValues(newValue.map((val) => Math.round(val)));
+  };
+
   return (
     <div className="w-11/12 md:w-4/5 mx-auto mt-10">
       {/* Top */}
@@ -14,13 +25,49 @@ const PropertyDetailsBanner = () => {
           <OvervieSection></OvervieSection>
         </div>
         {/* Right Side */}
-        <div className="lg:col-span-4 w-full h-80 bg-blue-400">
-          <div>
-            <p>Property Value</p>
-            <h3>$ 300k -$ 310k</h3>
-            <p>Your bid can not be than 10% of the property Minimum value.</p>
-            <form></form>
+        <div>
+          <div className="lg:col-span-4 w-full bg-[#ecf5ff] rounded-lg p-6 mb-6">
+            <p className="text-[#6B7280] text-lg">Property Value</p>
+            <h3 className="text-[#252323] font-bold text-2xl mt-1 mb-3">$ 300k -$ 310k</h3>
+            <p className="text-[#6B7280] text-lg font-medium leading-tight mb-8">Your bid can not be than 10% of the property Minimum value.</p>
+            <form>
+              <div>
+                <label>Min</label>
+                <input
+                  type="text"
+                  className=" rounded-md w-full py-3 px-4 mt-1"
+                  placeholder="Write minimum value"
+                  defaultValue="$ 280k"
+                />
+              </div>
+              <div className="mt-2">
+                <label>Max</label>
+                <input
+                  type="text"
+                  className=" rounded-md w-full py-3 px-4 mt-1"
+                  placeholder="Write minimum value"
+                  defaultValue="$ 305k"
+                />
+              </div>
+              <div className="mt-6 relative">
+                <RangeSlider
+                  id="range-slider-yellow"
+                  className="h-6 rounded-xl"
+                  step={"any"}
+                  min={280}
+                  max={305}
+                  value={value}
+                  onInput={handleInput}
+                />
+                <div className="absolute -bottom-7 transform -translate-x-1/2 text-black text-md font-medium left-6">$ {tooltipValues[0]}k</div>
+                <div className="absolute -bottom-7 transform -translate-x-1/2 text-black text-md font-medium -right-6">$ {tooltipValues[1]}k</div>
+              </div>
+              <div className="w-full flex items-center">
+                <button className="mt-12 rounded-md mx-auto bg-[#0059b1] text-white font-medium px-6 py-3">Bid Property</button>
+              </div>
+            </form>
           </div>
+          <div></div>
         </div>
       </div>
     </div>
