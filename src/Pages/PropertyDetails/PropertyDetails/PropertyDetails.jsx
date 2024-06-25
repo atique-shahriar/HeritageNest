39218d74eecde4075fda37bc5394da10d30665fa
atique-shahriar <satique06@gmail.com>
2024-06-25 21:26:24 +0600

    Create property details image section

---
+++ src/Pages/PropertyDetails/PropertyDetails/PropertyDetails.jsx
@@ -0,0 +1,11 @@
+import PropertyDetailsBanner from "../PropertyDetailsBanner/PropertyDetailsBanner";
+
+const PropertyDetails = () => {
+  return (
+    <div>
+      <PropertyDetailsBanner></PropertyDetailsBanner>
+    </div>
+  );
+};
+
+export default PropertyDetails;
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import BuyerScreen from "./Pages/BuyerScreens/BuyerScreen/BuyerScreen.jsx";
 import Landing from "./Pages/Landing/Landing/Landing.jsx";
+import PropertyDetails from "./Pages/PropertyDetails/PropertyDetails/PropertyDetails.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/",
+        element: <PropertyDetails></PropertyDetails>,
+      },
+      {
+        path: "/landingPage",
         element: <Landing></Landing>,
       },
       {
